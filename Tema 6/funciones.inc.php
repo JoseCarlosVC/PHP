f37d5124cec8921c1d5eb25112cc34f29addc9ab
JoseCarlosVC <9705@iespuertasdelcampo.es>
2021-11-08 14:06:55 +0100

    Funciones Tema 6

---
+++ Tema 6/funciones.inc.php
@@ -0,0 +1,109 @@
+<?php
+    function esCapicua($numero){
+        $primera_cifra = 0;
+        $ultima_cifra = 0;
+        $segunda_cifra = 0;
+        $penultima_cifra = 0;
+        $capicua = false;
+        //Sacamos la primera cifra
+        if($numero < 10){
+            $primera_cifra = $numero;
+            $ultima_cifra = $numero;
+        }else if(($numero >= 10) && ($numero <100)){
+            $primera_cifra = $numero / 10;
+            $primera_cifra = floor($primera_cifra);
+        }else if(($numero >= 100) && ($numero < 1000)){
+            $primera_cifra = $numero / 100;
+            $primera_cifra = floor($primera_cifra);
+        }else if(($numero >= 1000) && ($numero < 10000)){
+            $primera_cifra = $numero / 1000;
+            $primera_cifra = floor($primera_cifra);
+            //Necesitamos la segunda cifra de los números de 4 y 5 cifras
+            $segunda_cifra = $numero / 100;
+            $segunda_cifra = floor($segunda_cifra);
+            $segunda_cifra = $segunda_cifra % 10;
+            //Sacamos la penúltima cifra
+            $penultima_cifra = $numero % 100;
+            $penultima_cifra = $penultima_cifra / 10;
+            $penultima_cifra = floor($penultima_cifra);
+        }else if(($numero >= 10000) && ($numero < 100000)){
+            $primera_cifra = $numero / 10000;
+            $primera_cifra = floor($primera_cifra);
+            //Sacamos la segunda cifra
+            $segunda_cifra = $numero / 1000;
+            $segunda_cifra = floor($segunda_cifra);
+            $segunda_cifra = $segunda_cifra % 10;
+            //Sacamos la penúltima cifra
+            $penultima_cifra = $numero % 100;
+            $penultima_cifra = $penultima_cifra / 10;
+            $penultima_cifra = floor($penultima_cifra);
+        }
+        //Sacamos la última cifra
+        $ultima_cifra = $numero % 10;
+
+        if(($primera_cifra == $ultima_cifra) && ($segunda_cifra == $penultima_cifra)){
+            $capicua = true;
+        }else{
+            $capicua = false;
+        }
+        return $capicua;
+    }
+
+    function esPrimo($numero){
+        $es_primo = true;
+        //Empezamos el bucle en 2 y acabamos antes de llegar al numero introducido; asi si alguna vez es divisible sabremos que no es primo
+        //En caso de que sea positivo o negativo habra que usar un bucle diferente
+        if($numero >= 0){
+            for($i=2; $i<$numero; $i++){
+                if(($numero % $i) == 0){
+                    //Si en algun caso es divisible, se descarta
+                    $es_primo = false;
+                }
+            }
+        }else{
+            for($i=($numero+1); $i<-1; $i++){
+                if(($numero % $i) == 0){
+                    $es_primo = false;
+                }
+            }
+        }
+        return $es_primo;
+    }
+
+    function siguientePrimo($numero){
+        $primo = 0;
+        $es_primo = false;
+        $i = $numero;
+        while($primo == 0){
+            for($j=2; $i<$numero; $i++){
+                if(($numero % $i) == 0){
+                    $primo = $i;
+                }
+                $i++;
+            }
+        }
+        return $primo;
+    }
+
+    function potencia($base,$exponente){
+        $potencia = 1;
+        for($i = 0; $i < $exponente; $i++){
+            $potencia += $base * $base;
+        }
+        return $potencia;
+    }
+
+    function digitos($numero){
+        $digitos = 0;
+        $contar = $numero;
+        while($contar > 0){
+            $contar = $contar%10;
+            $digitos++;
+        }
+        return $digitos;
+    }
+
+    function voltea($numero){
+        
+    }
+?>
