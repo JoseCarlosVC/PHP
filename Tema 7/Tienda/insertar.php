88d3a78d50a1b09435c7e3e7280af802d6b26cd9
JoseCarlosVC <9705@iespuertasdelcampo.es>
2022-02-05 18:13:21 +0100

    cf

--- Tema 7/Tienda/insertar.php
+++ Tema 7/Tienda/insertar.php
@@ -1,24 +1,27 @@
 <?php
-    if(isset($_POST['enviar'])){
-        $comprobar = getimagesize($_FILES['imagen']['tmp_name']);
-        if($comprobar !== false){
-            $imagen = $_FILES['imagen']['tmp_name'];
-            $imgDatos = addslashes(file_get_contents($imagen));
 
-            require_once('./lib/conexion.inc.php');
-            $conexion = Conexion::openConexion();
-            $consulta = $conexion->stmt_init();
-        }
+if (isset($_POST['enviar'])) {
+    $comprobar = getimagesize($_FILES['imagen']['tmp_name']);
+    if ($comprobar !== false) {
+        $imagen = $_FILES['imagen']['tmp_name'];
+        $imgDatos = addslashes(file_get_contents($imagen));
+
+        include_once './lib/conexion.inc.php';
+        $conexion = Conexion::openConexion();
+        $consulta = $conexion->stmt_init();
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="#" method="POST" enctype="multipart/form-data">
         <label for="nombre">Introduce el nombre del producto: </label>
@@ -32,4 +35,5 @@
         <input type="submit" name="enviar" value="Subir">
     </form>
 </body>
+
 </html>
