e08918b7b16169d2ef55b787a5e3763180b28218
JoseCarlosVC <9705@iespuertasdelcampo.es>
2022-02-07 22:13:55 +0100

    ff

--- Tema 5/7-OrdenarPares/index.php
+++ Tema 5/7-OrdenarPares/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <link href="./../ejercicios.css" type="text/css" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
     <?php
         //Generamos el array
@@ -41,4 +43,5 @@
         }
     ?>
 </body>
+
 </html>
--- Tema 7/Tienda/detalles.php
+++ Tema 7/Tienda/detalles.php
@@ -18,15 +18,16 @@
         $detalles = $conexion->query("SELECT cod,nombre,descripcion,precio,imagen FROM producto WHERE cod=$cod");
         $stock = $detalles->fetch_assoc();
     ?>
-        <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($stock['imagen']); ?>" alt="" srcset="" width="250px" height="250px">
-        <br>
-        <span class="nombre"><?php echo $stock['nombre']; ?></span>
-        <br>
-        <span class="precio"><?php echo $stock['precio']; ?></span>
-        <br>
-        <span class="descripcion"><?php echo $stock['descripcion']; ?></span>
-        <br>
-        <a href="./tienda.php?cod=<?php echo $stock['cod']; ?>" class="comprar">Comprar</a>
+    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($stock['imagen']); ?>" alt="" srcset=""
+        width="250px" height="250px">
+    <br>
+    <span class="nombre"><?php echo $stock['nombre']; ?></span>
+    <br>
+    <span class="precio"><?php echo $stock['precio']; ?></span>
+    <br>
+    <span class="descripcion"><?php echo $stock['descripcion']; ?></span>
+    <br>
+    <a href="./tienda.php?cod=<?php echo $stock['cod']; ?>" class="comprar">Comprar</a>
 
     <?php
         $conexion = Conexion::closeConexion();
